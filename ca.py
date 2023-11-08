551283c7343574192f2f413f7e9ca2674093d606
BatesharXgit <bateshar.xyz@gmail.com>
2023-11-08 11:09:03 +0530

    ca

--- ca.py
+++ ca.py
@@ -1,44 +1,40 @@
-# Function to add an employee to the list
+#write a python program that store information about employees, including their names,
+#salary and departments, the program should allow user to add employees, find employees and update employees information.
+
 employees = []
 def add_employee(name, salary, department):
     employee = {"name": name, "salary": salary, "department": department}
     employees.append(employee)
 
-# Function to find an employee by name
+add_employee("Akshat", 50000, "HR")
+add_employee("Vimarsh", 60000, "Finance")
+add_employee("Rahul", 70000, "Marketing")
+
 def find_employee(name):
     for employee in employees:
         if employee["name"] == name:
             return employee
-    return None  # Employee not found
+    return None
 
-# Function to update employee information by name
 def update_employee(name, new_salary, new_department):
     for employee in employees:
         if employee["name"] == name:
-            employee["salary"] = new_salary
+            employee["Salary"] = new_salary
             employee["department"] = new_department
 
-# Example usage of the functions
-add_employee("Akshat", 50000, "HR")
-add_employee("Vimarsh", 60000, "Finance")
-add_employee("Vivek", 70000, "Marketing")
+print("List of employees are: ",employees)
 
-print("Employee List:")
-print(employees)
+employee_to_find = input("Enter name of employee you are looking for: ")
 
-# Find an employee by name
-employee_to_find = input("Enter the name of employee to find: ")
 found_employee = find_employee(employee_to_find)
 if found_employee:
     print(f"Found employee {employee_to_find}: {found_employee}")
 else:
     print(f"Employee {employee_to_find} not found.")
 
-# Update an employee's information
-employee_to_update = "Vimarsh"
-new_salary = 65000
-new_department = "Finance"
+employee_to_update = 'Rahul'
+new_salary = 80000
+new_department = 'Sales'
 update_employee(employee_to_update, new_salary, new_department)
 
-print("Updated Employee List:")
-print(employees)
+print("Updated employees list are: ", employees)
